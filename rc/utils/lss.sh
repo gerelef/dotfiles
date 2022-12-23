08a1ac82ef5e8d643441f67192d45766f56ecc0a
gerelef <44060598+gerelef@users.noreply.github.com>
2022-12-24 00:03:59 +0200

    moved unicode table function to lss to cleanup a little

--- rc/utils/lss.sh
+++ rc/utils/lss.sh
@@ -4,12 +4,43 @@
 DIR=$(dirname -- "$BASH_SOURCE")
 
 source "$DIR/math.sh"
-source "$DIR/_unicode-table.sh"
 source "$DIR/_git-branch.sh"
 
 shopt -s globstar
 shopt -s dotglob
 shopt -s nullglob
+
+_unicode_table () {
+    [[ -z "$@" ]] && return
+    local START_SYMBOL="┌"
+    local JOIN_SYMBOL="┬"
+    local ROW_SYMBOL="─"
+    local END_SYMBOL="┐"
+    if [[ "$1" == "--bot" ]]; then
+        local START_SYMBOL="└"
+        local JOIN_SYMBOL="┴"
+        local ROW_SYMBOL="─"
+        local END_SYMBOL="┘"
+    fi
+    
+    local sizes=()
+    
+    for arg in "${@:2}"; do
+        local sizes+=( "$arg" )
+    done
+    
+    local cc=1
+    echo -n "$START_SYMBOL"
+    for s in "${sizes[@]}"; do
+        for ((i=0; i<$s; ++i)); do
+            echo -n "$ROW_SYMBOL"
+        done
+        
+        [[ $cc -lt "${#sizes[@]}" ]] && echo -n "$JOIN_SYMBOL"
+        ((++cc))
+    done
+    echo "$END_SYMBOL"
+}
 
 lss () {
     # https://linuxhint.com/bash_operator_examples 
