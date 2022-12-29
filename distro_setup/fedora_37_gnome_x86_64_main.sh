8bda9f99ebcd70efa43d902b471a8fd6f8af0d19
gerelef <44060598+gerelef@users.noreply.github.com>
2022-12-29 12:34:23 +0200

    made everything use functions & global variables instead of hardcoded values...

--- distro_setup/fedora_37_gnome_x86_64_main.sh
+++ distro_setup/fedora_37_gnome_x86_64_main.sh
@@ -1,6 +1,89 @@
 #!/usr/bin/env bash
 
-# https://askubuntu.com/questions/425754/how-do-i-run-a-sudo-command-inside-a-script
+dnf-install () (
+    [[ $# -eq 0 ]] && return 2
+    
+    echo "-------------------DNF-INSTALL---------------- $*" | tr " " "\n"
+    while : ; do
+        dnf install -y "$*" && break
+    done
+    echo "Finished installing."
+)
+
+dnf-install-group () (
+    [[ $# -eq 0 ]] && return 2
+    
+    echo "-------------------DNF-GROUP-INSTALL---------------- $*" | tr " " "\n"
+    while : ; do
+        dnf groupinstall -y --with-optional "$*"
+    done
+    echo "Finished group-installing."
+)
+
+dnf-update-refresh () (
+    echo "-------------------DNF-UPDATE----------------"
+    while : ; do
+        dnf update -y --refresh && break
+    done
+    echo "Finished updating."
+)
+
+dnf-remove () (
+    [[ $# -eq 0 ]] && return 2
+    
+    echo "-------------------DNF-REMOVE---------------- $*" | tr " " "\n"
+    while : ; do
+        dnf remove -y "$*" && break
+    done
+    echo "Finished removing."
+)
+
+dnf-copr-enable () (
+    [[ $# -eq 0 ]] && return 2
+    
+    echo "-------------------DNF-ENABLE---------------- $*" | tr " " "\n"
+    while : ; do
+        dnf copr enable -y "$*"
+    done
+    echo "Finished adding from copr."
+)
+
+dnf-copr-remove () (
+    [[ $# -eq 0 ]] && return 2
+    
+    echo "-------------------DNF-REMOVE---------------- $*" | tr " " "\n"
+    while : ; do
+        dnf copr remove -y "$*"
+    done
+    echo "Finished adding copr repositories."
+)
+
+flatpak-install () (
+    [[ $# -eq 0 ]] && return 2
+    [[ -z "$REAL_USER" ]] && return 2
+    
+    echo "-------------------FLATPAK-INSTALL-SYSTEM---------------- $*" | tr " " "\n"
+    while : ; do
+        su - "$REAL_USER" -c "flatpak install --system -y $*" && break
+    done
+    echo "Finished flatpak-installing."
+)
+
+change-ownership () (
+    [[ $# -eq 0 ]] && return 2
+    [[ -z "$REAL_USER" ]] && return 2
+    
+    chown "$REAL_USER" "$*"
+    chmod 700 "$*"
+)
+
+change-ownership-recursive () (
+    [[ $# -eq 0 ]] && return 2
+    [[ -z "$REAL_USER" ]] && return 2
+    
+    chown -R "$REAL_USER" "$*"
+    chmod -R 700 "$*"
+)
 
 # ref: https://askubuntu.com/a/30157/8698
 if ! [ $(id -u) = 0 ]; then
@@ -14,44 +97,36 @@ if ! ping -q -c 1 -W 1 google.com > /dev/null; then
     exit 1
 fi
 
-
-# Define colors
-LIGHTGRAY="\033[0;37m"
-WHITE="\033[1;37m"
-BLACK="\033[0;30m"
-DARKGRAY="\033[1;30m"
-RED="\033[0;31m"
-LIGHTRED="\033[1;31m"
-GREEN="\033[0;32m"
-LIGHTGREEN="\033[1;32m"
-BROWN="\033[0;33m"
-YELLOW="\033[1;33m"
-BLUE="\033[0;34m"
-LIGHTBLUE="\033[1;34m"
-MAGENTA="\033[0;35m"
-LIGHTMAGENTA="\033[1;35m"
-CYAN="\033[0;36m"
-LIGHTCYAN="\033[1;36m"
-NOCOLOR="\033[0m"
-
-
 if [ $SUDO_USER ]; then
-    REAL_USER=$SUDO_USER
+    REAL_USER="$SUDO_USER"
 else
-    REAL_USER=$(whoami)
+    REAL_USER="$(whoami)"
 fi
 
 # https://unix.stackexchange.com/questions/247576/how-to-get-home-given-user
 REAL_USER_HOME=$(eval echo "~$REAL_USER")
+# dotfiles directories
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 RC_DIR="$SCRIPT_DIR/../rc"
 RC_MZL_DIR="$SCRIPT_DIR/../firefox"
 
+# home directories to create
+CLONED_ROOT="$REAL_USER_HOME/cloned"
+MZL_ROOT="$REAL_USER_HOME/.mozilla/firefox"
+SSH_ROOT="$REAL_USER_HOME/.ssh"
+BIN_ROOT="$REAL_USER_HOME/bin"
+WRK_ROOT="$REAL_USER_HOME/work"
+SMR_ROOT="$REAL_USER_HOME/seminar"
+RND_ROOT="$REAL_USER_HOME/random"
+
+# there should be a matching change-ownership-recursive after everything's done in the script 
+mkdir -p "$CLONED_ROOT" "$MZL_ROOT" "$SSH_ROOT" "$BIN_ROOT" "$WRK_ROOT" "$SMR_ROOT" "$RND_ROOT" 
+
 # fs thingies
 ROOT_FS=$(stat -f --format=%T /)
-REAL_USER_HOME_FS=$(stat -f --format=%T $REAL_USER_HOME)
-SCRIPT_DIR_FS=$(stat -f --format=%T $SCRIPT_DIR)
+REAL_USER_HOME_FS=$(stat -f --format=%T "$REAL_USER_HOME")
+SCRIPT_DIR_FS=$(stat -f --format=%T "$SCRIPT_DIR")
 
 DISTRIBUTION_NAME="fedora"
 
@@ -173,6 +248,11 @@ gnome-shell-extension-appindicator \
 gnome-shell-extension-dash-to-panel \
 "
 
+INSTALLABLE_BTRFS_TOOLS="\
+btrfs-assistant \
+timeshift \
+"
+
 INSTALLABLE_IDE_FLATPAKS="\
 org.gnome.Builder \
 ar.xjuan.Cambalache \
@@ -180,10 +260,24 @@ cc.arduino.IDE2 \
 com.vscodium.codium \
 "
 
+INSTALLABLE_NVIDIA_DRIVERS="\
+akmod-nvidia \
+xorg-x11-drv-nvidia-cuda \
+"
+
+COPR_REPOSITORIES_AVAILABLE="\
+nickavem/adw-gtk3 \
+"
+
+COPR_REPOSITORIES_REMOVABLE="\
+phracek/PyCharm \
+"
+
+#######################################################################################################
+
 # https://github.com/tommytran732/Linux-Setup-Scripts/blob/main/Fedora-Workstation-36.sh
 # Make home directory private
-chmod 700 "$REAL_USER_HOME"
-chown "$REAL_USER" "$REAL_USER_HOME"
+change-ownership "$REAL_USER_HOME"
 systemctl enable fstrim.timer
 
 #######################################################################################################
@@ -195,45 +289,36 @@ command cp -r "$RC_DIR/dnf.conf" "/etc/dnf/dnf.conf"
 chown root "/etc/dnf/dnf.conf"
 chmod 644 "/etc/dnf/dnf.conf"
 
-echo "Finished copying dnf.conf."
+echo "Finished copying dnf.conf"
 
 #######################################################################################################
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
-flatpak remote-add --if-not-exists appcenter https://flatpak.elementary.io/repo.flatpakrepo
-dnf copr remove -y phracek/PyCharm
-dnf copr enable -y nickavem/adw-gtk3
+dnf-copr-enable "$COPR_REPOSITORIES_AVAILABLE"
+dnf-copr-remove "$COPR_REPOSITORIES_REMOVABLE"
 
 #######################################################################################################
-
-echo "-------------------UPDATING----------------"
-while : ; do
-    dnf update -y --refresh && break
-done
-echo "Finished updating system."
-
-#######################################################################################################
+dnf-update-refresh
 
 fwupdmgr refresh --force -y
 fwupdmgr get-updates -y
 fwupdmgr update -y
 
+#######################################################################################################
+
 echo "-------------------INSTALLING---------------- $INSTALLABLE_PACKAGES $INSTALLABLE_CODECS $INSTALLABLE_BASHRC_DEPENDENCIES" | tr " " "\n"
+dnf-remove "$UNINSTALLABLE_BLOAT"
+dnf-install "$INSTALLABLE_PACKAGES"
+dnf-install "$INSTALLABLE_CODECS"
+dnf-install "$INSTALLABLE_BASHRC_DEPENDENCIES"
 while : ; do
-    dnf remove -y $UNINSTALLABLE_BLOAT || continue
-    dnf install -y $INSTALLABLE_PACKAGES || continue
-    dnf install -y $INSTALLABLE_CODECS || continue
-    dnf install -y $INSTALLABLE_BASHRC_DEPENDENCIES || continue
-    dnf group upgrade -y --with-optional Multimedia || continue
-    break
+    dnf group upgrade -y --with-optional Multimedia || break
 done
 
 case "btrfs" in
     "$ROOT_FS" | "$REAL_USER_HOME_FS" |  "$SCRIPT_DIR_FS")
-        echo "Found BTRFS, installing btrfs-assistant"
-        dnf install -y btrfs-assistant
-        echo "Finished installing btrfs-assistant"
+        echo "Found BTRFS, installing tools"
+        dnf-install "$INSTALLABLE_BTRFS_TOOLS"
         ;;
     *)
         echo "BTRFS not found; continuing as usual..."
@@ -242,20 +327,18 @@ esac
 
 GPU=$(lspci | grep -i vga | grep NVIDIA)
 if [ ! -z "$GPU" ]; then
-    echo "Found NVIDIA GPU $GPU, installing latest drivers..."
-    dnf install akmod-nvidia
-    dnf install xorg-x11-drv-nvidia-cuda
-    echo "Finished installing latest drivers."
+    echo "Found NVIDIA GPU $GPU, installing drivers..."
+    dnf-install "$INSTALLABLE_NVIDIA_DRIVERS"
     echo "Signing GPU drivers..."
     /usr/sbin/kmodgenca
     mokutil --import /etc/pki/akmods/certs/public_key.der
-    echo "Signed GPU drivers."
+    echo "Finished signing GPU drivers."
 fi
 
 echo "Switching to $REAL_USER to install flatpaks"
 echo "-------------------INSTALLING---------------- $INSTALLABLE_FLATPAKS $INSTALLABLE_OBS_STUDIO" | tr " " "\n"
-su - $REAL_USER -c "flatpak --system install -y $INSTALLABLE_FLATPAKS"
-su - $REAL_USER -c "flatpak --system install -y $INSTALLABLE_OBS_STUDIO"
+flatpak-install "$INSTALLABLE_FLATPAKS"
+flatpak-install "$INSTALLABLE_OBS_STUDIO"
 echo "Continuing as $(whoami)"
 
 #######################################################################################################
@@ -268,11 +351,7 @@ while : ; do
 done
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    while : ; do
-        dnf groupinstall -y "Development Tools" || continue
-        break
-    done
-    echo "Finished installing Development Tools."
+    dnf-install-group "Development Tools"
 fi
 
 #######################################################################################################
@@ -280,17 +359,15 @@ fi
 echo "-------------------INSTALLING---------------- $INSTALLABLE_IDE_FLATPAKS" | tr " " "\n"
 while : ; do
     read -p "Are you sure you want to install Community IDEs & Jetbrains Toolbox?[Y/n] " -n 1 -r
-    ! [[ $REPLY =~ ^[YyNn]$ ]] || break
+    [[ ! $REPLY =~ ^[YyNn]$ ]] || break
 done
 
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    su - $REAL_USER -c "flatpak --system install -y $INSTALLABLE_IDE_FLATPAKS"
+    flatpak-install "$INSTALLABLE_IDE_FLATPAKS"
     echo "Finished installing IDEs."
 
-    echo "-------------------INSTALLING JETBRAINS TOOLBOX----------------"
-    mkdir -p "$REAL_USER_HOME/cloned"
-    chown -R "$REAL_USER" "$REAL_USER_HOME/cloned"
+    echo "-------------------INSTALLING JETBRAINS TOOLBOX----------------" 
     curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
     echo "Finished installing toolbox."
 fi
@@ -305,66 +382,48 @@ done
 echo ""
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    while : ; do
-        dnf install -y $INSTALLABLE_EXTENSIONS
-        [[ $? == 0 ]] && continue
-        break
-    done
-
-    echo "Finished installing extensions."
+    dnf-install "$INSTALLABLE_EXTENSIONS"
 fi
 
 #######################################################################################################
 
-echo "Make sure to run firefox at least once; finishing install in 60 seconds..."
-sleep 60
+echo "Running firefox as user to create it's configuration directories; let it load fully, then close it..."
+su - "$REAL_USER" -c "firefox"
 
 #######################################################################################################
 
 echo "-------------------INSTALLING RC FILES----------------"
 
 cat "$RC_DIR/mimeapps.list" >> "$REAL_USER_HOME/.config/mimeapps.list"
-chown "$REAL_USER" "$REAL_USER_HOME/.config/mimeapps.list"
-chmod 700 "$REAL_USER_HOME/.config/mimeapps.list"
+change-ownership "$REAL_USER_HOME/.config/mimeapps.list"
 
 touch "$REAL_USER_HOME/.bashrc_private"
-chown "$REAL_USER" "$REAL_USER_HOME/.bashrc_private"
-chmod 700 "$REAL_USER_HOME/.bashrc_private"
+change-ownership "$REAL_USER_HOME/.bashrc_private"
 
 ln -sf "$RC_DIR/.vimrc" "$REAL_USER_HOME/.vimrc"
 ln -sf "$RC_DIR/.bashrc" "$REAL_USER_HOME/.bashrc"
 ln -sf "$RC_DIR/.nanorc" "$REAL_USER_HOME/.nanorc"
 ln -sf "$RC_DIR/.gitconfig" "$REAL_USER_HOME/.gitconfig"
-chown "$REAL_USER" "$REAL_USER_HOME/.vimrc"
-chmod 700 "$REAL_USER_HOME/.vimrc"
-chown "$REAL_USER" "$REAL_USER_HOME/.bashrc"
-chmod 700 "$REAL_USER_HOME/.bashrc"
-chown "$REAL_USER" "$REAL_USER_HOME/.nanorc"
-chmod 700 "$REAL_USER_HOME/.nanorc"
-chown "$REAL_USER" "$REAL_USER_HOME/.gitconfig"
-chmod 700 "$REAL_USER_HOME/.gitconfig"
+change-ownership "$REAL_USER_HOME/.vimrc" "$REAL_USER_HOME/.bashrc" "$REAL_USER_HOME/.nanorc" "$REAL_USER_HOME/.gitconfig"
 
-mkdir -p "$REAL_USER_HOME/cloned/mono-firefox-theme"
-echo "Created $REAL_USER_HOME/cloned/mono-firefox-theme/"
-RC_VIS_MZL_DIR="$REAL_USER_HOME/cloned/mono-firefox-theme"
+mkdir -p "$CLONED_ROOT/mono-firefox-theme"
+echo "Created $CLONED_ROOT/mono-firefox-theme/"
+RC_VIS_MZL_DIR="$CLONED_ROOT/mono-firefox-theme"
 while : ; do
     # if something goes wrong, install the next version, otherwise break
-    wget --directory-prefix "$REAL_USER_HOME/cloned/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.4/mono-firefox-theme.tar.xz" && break
-    wget --directory-prefix "$REAL_USER_HOME/cloned/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.3/mono-firefox-theme.tar.xz" && break
-    wget --directory-prefix "$REAL_USER_HOME/cloned/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.2/mono-firefox-theme.tar.xz" && break
-    wget --directory-prefix "$REAL_USER_HOME/cloned/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.1/mono-firefox-theme.tar.xz"
+    wget --directory-prefix "$CLONED_ROOT/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.4/mono-firefox-theme.tar.xz" && break
+    wget --directory-prefix "$CLONED_ROOT/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.3/mono-firefox-theme.tar.xz" && break
+    wget --directory-prefix "$CLONED_ROOT/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.2/mono-firefox-theme.tar.xz" && break
+    wget --directory-prefix "$CLONED_ROOT/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.1/mono-firefox-theme.tar.xz"
     break
 done
-tar -xf "$REAL_USER_HOME/cloned/mono-firefox-theme.tar.xz" --directory="$RC_VIS_MZL_DIR"
-echo "Extracted $REAL_USER_HOME/cloned/mono-firefox-theme.tar.xz"
-rm -vf "$REAL_USER_HOME/cloned/mono-firefox-theme.tar.xz"
+tar -xf "$CLONED_ROOT/mono-firefox-theme.tar.xz" --directory="$RC_VIS_MZL_DIR"
+echo "Extracted $CLONED_ROOT/mono-firefox-theme.tar.xz"
+rm -vf "$CLONED_ROOT/mono-firefox-theme.tar.xz"
 cat "$RC_MZL_DIR/userChrome.css" >> "$RC_VIS_MZL_DIR/userChrome.css"
 echo "Installing visual rc files from $RC_VIS_MZL_DIR"
-chown -R "$REAL_USER" "$RC_VIS_MZL_DIR/"
-chmod -R 700 "$RC_VIS_MZL_DIR/"
 
 #https://askubuntu.com/questions/239543/get-the-default-firefox-profile-directory-from-bash
-MZL_ROOT="$REAL_USER_HOME/.mozilla/firefox"
 if [[ $(grep '\[Profile[^0]\]' "$MZL_ROOT/profiles.ini") ]];then
     PROFPATH=$(grep -E '^\[Profile|^Path|^Default' "$MZL_ROOT/profiles.ini" | grep '^Path' | cut -c6- | tr " " "\n")
 else
@@ -377,41 +436,26 @@ for MZL_PROF_DIR in $PROFPATH; do
 
     # preference rc
     ln -sf "$RC_MZL_DIR/user.js" "$MZL_PROF_DIR_ABSOLUTE/user.js"
-    chown -R "$REAL_USER" "$MZL_PROF_DIR_ABSOLUTE/user.js"
-    chmod 700 "$MZL_PROF_DIR_ABSOLUTE/user.js"
+    change-ownership "$MZL_PROF_DIR_ABSOLUTE/user.js"
 
     # visual rc
     mkdir -p "$MZL_PROF_CHROME_DIR_ABSOLUTE"
     ln -sf "$RC_VIS_MZL_DIR/userChrome.css" "$MZL_PROF_CHROME_DIR_ABSOLUTE/userChrome.css"
     ln -sf "$RC_VIS_MZL_DIR/userContent.css" "$MZL_PROF_CHROME_DIR_ABSOLUTE/userContent.css"
     ln -sf "$RC_VIS_MZL_DIR/mono-firefox-theme" "$MZL_PROF_CHROME_DIR_ABSOLUTE/mono-firefox-theme"
-    chown -R "$REAL_USER" "$MZL_PROF_CHROME_DIR_ABSOLUTE"
-    chmod 700 "$MZL_PROF_CHROME_DIR_ABSOLUTE"
 done
-
 echo "Finished installing rc files."
 
 #######################################################################################################
 
-mkdir -p "$REAL_USER_HOME/.ssh"
-ssh-keygen -t rsa -b 4096 -C "$REAL_USER@$DISTRIBUTION_NAME" -f "$REAL_USER_HOME/.ssh/id_rsa" -P "" && cat "$REAL_USER_HOME/.ssh/id_rsa.pub"
-chown -R "$REAL_USER" "$REAL_USER_HOME/.ssh"
-chmod 700 "$REAL_USER_HOME/.ssh"
+ssh-keygen -t rsa -b 4096 -C "$REAL_USER@$DISTRIBUTION_NAME" -f "$SSH_ROOT/id_rsa" -P "" && cat "$SSH_ROOT/id_rsa.pub"
 
 #######################################################################################################
 
-mkdir -p "$REAL_USER_HOME/bin"
-mkdir -p "$REAL_USER_HOME/work"
-mkdir -p "$REAL_USER_HOME/seminar"
-mkdir -p "$REAL_USER_HOME/random"
-chmod 700 "$REAL_USER_HOME/bin"
-chown -R "$REAL_USER" "$REAL_USER_HOME/bin"
-chmod 700 "$REAL_USER_HOME/work"
-chown -R "$REAL_USER" "$REAL_USER_HOME/work"
-chmod 700 "$REAL_USER_HOME/seminar"
-chown -R "$REAL_USER" "$REAL_USER_HOME/seminar"
-chmod 700 "$REAL_USER_HOME/random"
-chown -R "$REAL_USER" "$REAL_USER_HOME/random"
+#matching the mkdir
+change-ownership-recursive "$CLONED_ROOT" "$MZL_ROOT" "$SSH_ROOT" "$BIN_ROOT" "$WRK_ROOT" "$SMR_ROOT" "$RND_ROOT"
+
+#######################################################################################################
 
 systemctl restart NetworkManager
 hostnamectl hostname "$DISTRIBUTION_NAME"
@@ -427,11 +471,6 @@ fi
 echo "--------------------------- VISUDO ---------------------------"
 echo "Please sudo visudo and add:"
 echo "  Defaults env_reset, timestamp_timeout=120, pwfeedback"
-echo "--------------------------- SELINUX ---------------------------"
-echo "Please sudo nano /etc/sysconfig/selinux and set:"
-echo "Note: SELinux is not recommended to be disabled."
-echo "  SELINUX=permissive"
-echo "  SELINUXTYPE=targeted"
 echo "--------------------------- THEMING ---------------------------"
 echo "Please install 'night theme switcher' using the Gnome Extensions website."
 echo "https://extensions.gnome.org/extension/2236/night-theme-switcher/"
@@ -439,12 +478,6 @@ echo ""
 echo "Please run AdwSteamGtk to convert steam to the Adwaita theme."
 echo ""
 echo "Please go to firefox about:config and enable toolkit.legacyUserProfileCustomizations.stylesheets to true."
-echo "--------------------------- IDE ---------------------------"
-echo "Recommended Jetbrains IDEs are:"
-echo "- PyCharm Ultimate"
-echo "- IntelliJ Idea Ultimate"
-echo "- CLion"
-echo "- Android Studio"
 echo "--------------------------- FSTAB ---------------------------"
 echo "Remember to add a permanent mount point for permanently mounted partitions."
 echo "Standard fstab USER mount arguments:"
