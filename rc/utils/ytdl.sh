326002acc1abf261774d929a217b31a102d7225b
gerelef <44060598+gerelef@users.noreply.github.com>
2023-05-03 15:53:12 +0300

    converted appropriate bash functions to subshell

--- rc/utils/ytdl.sh
+++ rc/utils/ytdl.sh
@@ -7,24 +7,24 @@ readonly __YTDL_LOADED="__LOADED"
 REQUIRE_DEPENDENCIES+="yt-dlp yt-dlp-bash-completion "
 
 # yt-dlp download to mp3
-ytdl-mp3 () {
+ytdl-mp3 () (
     [[ -z "$*" ]] && return 2 
     
     yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 "$@" 
-}
+)
 
 # yt-dlp download to mp4
-ytdl-mp4 () {
+ytdl-mp4 () (
     [[ -z "$*" ]] && return 2
     
     yt-dlp --format "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]" "$@"
-}
+)
 
-ytdl-thumbnail () {
+ytdl-thumbnail () (
     [[ -z "$*" ]] && return 2
     
     yt-dlp --write-thumbnail --skip-download "$@"
-}
+)
 
 export -f ytdl-mp3
 export -f ytdl-mp4
