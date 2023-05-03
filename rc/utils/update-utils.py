9294ac64bc13ab528b11e5285a92433679ee5dd6
gerelef <44060598+gerelef@users.noreply.github.com>
2023-05-03 21:10:28 +0300

    added python utilities to update stuff

--- rc/utils/update-utils.py
+++ rc/utils/update-utils.py
@@ -1,50 +1,6 @@
 #!/usr/bin/env python3
-
-# TODO
-def run_subprocess(commands, cwd) -> bool:
-    """Runs a number of Commands in  the Currently Working Directory"""
-    return sp.run(commands, cwd=cwd).returncode) == 0
-
-# TODO
-def get_github_releases(url, recurse=False) -> bool:
-    """Gets all the github releases for a given match, and return the appropriate class. Can use github release paging to find all compatible releases."""
-    return True
-
-
-def download(url, chunk_size=1024*1024) -> tuple(int, int, bin):
-    """Generator that returns a binary data packet of size chunk_size, requested from url. First returnee is the currently read bytes, second is the total read bytes, and the third is the actual data itself. """
-    import requests
-    with requests.get(url, verify=True, stream=True, allow_redirects=True) as req:
-        btotal = int(req.headers.get('content-length'))
-        bread = 0
-        for data in req.iter_content(chunk_size=chunk_size):
-            bread += len(data)
-            yield bread, btotal, data
-    return 
-
-
-def echo_progress_bar_simple(current, total, stream):
-    """Echo a simple percentage in the stream. Stream must be a stream of type TextIOWrapper, or any other class that has a .write(str) and .flush() method."""
-    stream.write(f"\r{round((bread/btotal)*100, 2)}%")
-    stream.flush()
-
-
-# TODO
-def echo_progress_bar_complex(current, total, stream, max_columns, use_ascii=False):
-    """Echo a complex progress bar in the stream. Stream must be a stream of type TextIOWrapper, or any other class that has a .write(str) and .flush() method."""
-    pass
-
-
-def is_root() -> bool:
-    """Returns True if script is running as root."""
-    import os
-    return os.geteuid() == 0
-
-
-def get_all_subdirectories(path) -> list[str]:
-    """Returns the filenames of all subdirectories in a path."""
-    import os
-    return os.listdir(path=path)
+from dataclasses import dataclass
+from typing import Generator, List
 
 
 # Writing boilerplate code to avoid writing boilerplate code!
@@ -110,3 +66,168 @@ def auto_hash(cls):
 
     return cls
 
+
+@auto_str
+@auto_eq
+@dataclass
+class __Release:
+    id: int
+    author_login: str
+
+    tag_name: str
+    name: str
+
+    body: str
+    isDraft: bool
+    isPrerelease: bool
+    created_at: str
+    published_at: str
+
+    assets: dict[str, str]  # filename, link
+
+
+def run_subprocess(commands, cwd) -> bool:
+    """Runs a number of Commands in  the Currently Working Directory"""
+    import subprocess
+    from os import path
+    return subprocess.run(commands, cwd=path.expanduser(cwd)).returncode == 0
+
+
+def get_github_releases(url, recurse=False) -> list[__Release] | None:
+    """Gets all the GitHub releases for a given match, and return a list of Release class. Can use GitHub release
+    paging to find all compatible releases."""
+    from datetime import datetime
+    import requests
+
+    releases: list[__Release] = []
+    while True:
+        try:
+            with requests.get(url, verify=True) as req:
+                if req.status_code != 200:
+                    print(f"Got status code {req.status_code}", file=sys.stderr)
+                    exit(1)
+                releases_recvd = req.json()
+                releases_links = req.links
+
+            for version in releases_recvd:
+                try:
+                    assets = {}
+                    for asset in version["assets"]:
+                        assets[asset["name"]] = asset["browser_download_url"]
+
+                    releases.append(
+                        __Release(
+                            id=int(version["id"]),
+                            author_login=version["author"]["login"],
+                            tag_name=version["tag_name"],
+                            name=version["name"],
+                            body=version["body"],
+                            isDraft=bool(version["draft"]),
+                            isPrerelease=bool(version["prerelease"]),
+                            # https://stackoverflow.com/a/36236080/10007109
+                            created_at=datetime.strptime(version["created_at"], "%Y-%m-%dT%H:%M:%SZ"),
+                            published_at=datetime.strptime(version["published_at"], "%Y-%m-%dT%H:%M:%SZ"),
+                            assets=assets
+                        )
+                    )
+                except IndexError:
+                    pass
+
+            if not recurse:
+                break
+
+            url = releases_links['next']['url']
+        except KeyError:
+            # if either next links don't exist, we're done
+            break
+        except requests.exceptions.JSONDecodeError:
+            # if the json has errored return None
+            return None
+
+    return releases
+
+
+def download(url, chunk_size=1024 * 1024) -> Generator[int, int, bin]:
+    """Generator that returns a binary data packet of size chunk_size, requested from url. First returnee is the
+    currently read bytes, second is the total read bytes, and the third is the actual data itself."""
+    import requests
+    with requests.get(url, verify=True, stream=True, allow_redirects=True) as req:
+        btotal = int(req.headers.get('content-length'))
+        bread = 0
+        for data in req.iter_content(chunk_size=chunk_size):
+            bread += len(data)
+            yield bread, btotal, data
+    return
+
+
+def echo_progress_bar_simple(current, total, stream):
+    """Echo a simple percentage in the stream. Stream must be a stream of type TextIOWrapper, or any other class that
+    has a .write(str) and .flush() method."""
+    stream.write(f"\r{round((current / total) * 100, 2)}%")
+    stream.flush()
+
+
+def echo_progress_bar_complex(current, total, stream, max_columns, use_ascii=False):
+    """Echo a complex progress bar in the stream. Stream must be a stream of type TextIOWrapper, or any other class
+    that has a .write(str) and .flush() method."""
+    empty_space = " " if use_ascii else "\033[1m\033[38;5;196m―\033[0m"  # bold, light_red & clean_colour
+    filled_space = "-" if use_ascii else "\033[1m\033[38;5;34m―\033[0m"  # bold, green & clean_colour
+    # total bar length: we're going to use the max columns with a padding of 6 characters
+    #  for the "[" "]" "999%" pads.
+    bar_length = max_columns - 6
+    bar = ["\r", "["] + [empty_space] * bar_length + ["]"] + list(f"\033[1m\033[38;5;34m{round((current / total) * 100, 1)}%\033[0m")
+    for i in range(2, bar_length + 2):
+        if round(i / (bar_length + 1), 2) <= round(current / total, 2):
+            bar[i] = filled_space
+    stream.write("".join(bar))
+    stream.flush()
+
+
+def is_root() -> bool:
+    """Returns True if script is running as root."""
+    import os
+    return os.geteuid() == 0
+
+
+def get_all_subdirectories(path) -> list[str]:
+    """Returns the filenames of all subdirectories in a path."""
+    import os
+    return os.listdir(path=path)
+
+
+if __name__ == "__main__":
+    from time import sleep
+    import sys
+
+    print(f"Script is running as {'root' if is_root() else 'user'}")
+
+    for i in range(100):
+        echo_progress_bar_simple(i, 99, sys.stdout)
+        sleep(0.1)
+
+    print()
+
+    for i in range(100):
+        echo_progress_bar_complex(i, 99, sys.stdout, 50)
+        sleep(0.1)
+
+    print()
+
+    for i in range(100):
+        echo_progress_bar_complex(i, 99, sys.stdout, 16)
+        sleep(0.1)
+
+    print()
+
+    for release in get_github_releases("https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases"):
+        print(release.name)
+        sleep(0.1)
+
+    for release in get_github_releases(
+            "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases",
+            recurse=True):
+        print(release)
+        sleep(0.1)
+
+    if run_subprocess(["echo", "hello", "bash!" "$PWD"], "~"):
+        print("Success!")
