43cf73e99f57b9aa789d037b564a9e0fb9164a42
gerelef <44060598+gerelef@users.noreply.github.com>
2023-06-08 14:54:21 +0300

    moved all environment variables for dotfiles/appliction configs to common-utils.sh as they should be

--- scripts/setup/common-utils.sh
+++ scripts/setup/common-utils.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/env bash
 
+if [[ -n "$__UTILS_LOADED" ]]; then
+    return 0
+fi
+readonly __UTILS_LOADED="__LOADED"
+
 shopt -s globstar
 shopt -s dotglob
 shopt -s nullglob
+
+if [ $SUDO_USER ]; then
+    REAL_USER="$SUDO_USER"
+else
+    REAL_USER="$(whoami)"
+fi
+
+# https://unix.stackexchange.com/questions/247576/how-to-get-home-given-user
+readonly REAL_USER_HOME=$(eval echo "~$REAL_USER")
+# dotfiles directories
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+readonly RC_DIR="$SCRIPT_DIR/.."
+readonly CONFIG_DIR="$SCRIPT_DIR/../../.config"
+readonly RC_MZL_DIR="$CONFIG_DIR/mozilla"
+
+# home directories to create
+readonly CLONED_ROOT="$REAL_USER_HOME/cloned"
+readonly MZL_ROOT="$REAL_USER_HOME/.mozilla/firefox"
+readonly SSH_ROOT="$REAL_USER_HOME/.ssh"
+readonly BIN_ROOT="$REAL_USER_HOME/bin"
+readonly WRK_ROOT="$REAL_USER_HOME/work"
+readonly SMR_ROOT="$REAL_USER_HOME/seminar"
+readonly RND_ROOT="$REAL_USER_HOME/random"
 
 dnf-install () (
     [[ $# -eq 0 ]] && return 2
@@ -66,3 +95,59 @@ change-ownership-recursive () (
     chown -R "$REAL_USER" "$@"
     chmod -R 700 "$@"
 )
+
+copy-dnf () (
+    command cp -r "$CONFIG_DIR/dnf.conf" "/etc/dnf/dnf.conf"
+    chown root "/etc/dnf/dnf.conf"
+    chmod 644 "/etc/dnf/dnf.conf"
+)
+
+create-private-bashrc () (
+    touch "$REAL_USER_HOME/.bashrc_private"
+    change-ownership "$REAL_USER_HOME/.bashrc_private"
+)
+
+copy-rc-files () (
+    ln -sf "$RC_DIR/.vimrc" "$REAL_USER_HOME/.vimrc"
+    ln -sf "$RC_DIR/.bashrc" "$REAL_USER_HOME/.bashrc"
+    ln -sf "$RC_DIR/.nanorc" "$REAL_USER_HOME/.nanorc"
+    ln -sf "$CONFIG_DIR/.gitconfig" "$REAL_USER_HOME/.gitconfig"
+    change-ownership "$REAL_USER_HOME/.vimrc" "$REAL_USER_HOME/.bashrc" "$REAL_USER_HOME/.nanorc" "$REAL_USER_HOME/.gitconfig"
+)
+
+copy-ff-rc-files () (
+    mkdir -p "$CLONED_ROOT/mono-firefox-theme"
+    echo "Created $CLONED_ROOT/mono-firefox-theme/"
+    readonly RC_VIS_MZL_DIR="$CLONED_ROOT/mono-firefox-theme"
+    # if something goes wrong, install the next version, otherwise break
+    wget -c --read-timeout=5 --tries=0 --directory-prefix "$CLONED_ROOT/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.5/mono-firefox-theme.tar.xz"
+    tar -xf "$CLONED_ROOT/mono-firefox-theme.tar.xz" --directory="$RC_VIS_MZL_DIR"
+    echo "Extracted $CLONED_ROOT/mono-firefox-theme.tar.xz"
+    rm -vf "$CLONED_ROOT/mono-firefox-theme.tar.xz"
+    cat "$RC_MZL_DIR/userChrome.css" >> "$RC_VIS_MZL_DIR/userChrome.css"
+    echo "Installing visual rc files from $RC_VIS_MZL_DIR"
+
+    #https://askubuntu.com/questions/239543/get-the-default-firefox-profile-directory-from-bash
+    if [[ $(grep '\[Profile[^0]\]' "$MZL_ROOT/profiles.ini") ]]; then
+        readonly PROFPATH=$(grep -E '^\[Profile|^Path|^Default' "$MZL_ROOT/profiles.ini" | grep '^Path' | cut -c6- | tr " " "\n")
+    else
+        readonly PROFPATH=$(grep 'Path=' "$MZL_ROOT/profiles.ini" | sed 's/^Path=//')
+    fi
+
+    for MZL_PROF_DIR in $PROFPATH; do
+        MZL_PROF_DIR_ABSOLUTE="$MZL_ROOT/$MZL_PROF_DIR"
+        MZL_PROF_CHROME_DIR_ABSOLUTE="$MZL_PROF_DIR_ABSOLUTE/chrome"
+
+        # preference rc
+        ln -sf "$RC_MZL_DIR/user.js" "$MZL_PROF_DIR_ABSOLUTE/user.js"
+        change-ownership "$MZL_PROF_DIR_ABSOLUTE/user.js"
+
+        # visual rc
+        mkdir -p "$MZL_PROF_CHROME_DIR_ABSOLUTE"
+        ln -sf "$RC_VIS_MZL_DIR/userChrome.css" "$MZL_PROF_CHROME_DIR_ABSOLUTE/userChrome.css"
+        ln -sf "$RC_VIS_MZL_DIR/userContent.css" "$MZL_PROF_CHROME_DIR_ABSOLUTE/userContent.css"
+        ln -sf "$RC_VIS_MZL_DIR/mono-firefox-theme" "$MZL_PROF_CHROME_DIR_ABSOLUTE/mono-firefox-theme"
+    done
+)
+
+
--- scripts/setup/fedora_38_gnome_x86_64_main.sh
+++ scripts/setup/fedora_38_gnome_x86_64_main.sh
@@ -16,38 +16,12 @@ if ! ping -q -c 1 -W 1 google.com > /dev/null; then
     exit 1
 fi
 
-if [ $SUDO_USER ]; then
-    REAL_USER="$SUDO_USER"
-else
-    REAL_USER="$(whoami)"
-fi
-
-# https://unix.stackexchange.com/questions/247576/how-to-get-home-given-user
-readonly REAL_USER_HOME=$(eval echo "~$REAL_USER")
-# dotfiles directories
-# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-readonly RC_DIR="$SCRIPT_DIR/.."
-readonly CONFIG_DIR="$SCRIPT_DIR/../../.config"
-readonly RC_MZL_DIR="$CONFIG_DIR/mozilla"
-
-# home directories to create
-readonly CLONED_ROOT="$REAL_USER_HOME/cloned"
-readonly MZL_ROOT="$REAL_USER_HOME/.mozilla/firefox"
-readonly SSH_ROOT="$REAL_USER_HOME/.ssh"
-readonly BIN_ROOT="$REAL_USER_HOME/bin"
-readonly WRK_ROOT="$REAL_USER_HOME/work"
-readonly SMR_ROOT="$REAL_USER_HOME/seminar"
-readonly RND_ROOT="$REAL_USER_HOME/random"
-
 # there should be a matching change-ownership-recursive after everything's done in the script 
 mkdir -p "$CLONED_ROOT" "$MZL_ROOT" "$SSH_ROOT" "$BIN_ROOT" "$WRK_ROOT" "$SMR_ROOT" "$RND_ROOT" 
 
 # fs thingies
 readonly ROOT_FS=$(stat -f --format=%T /)
 readonly REAL_USER_HOME_FS=$(stat -f --format=%T "$REAL_USER_HOME")
-readonly SCRIPT_DIR_FS=$(stat -f --format=%T "$SCRIPT_DIR")
-
 readonly DISTRIBUTION_NAME="fedora"
 
 readonly INSTALLABLE_PACKAGES="\
@@ -202,9 +176,7 @@ systemctl enable fstrim.timer
 echo "-------------------DNF.CONF----------------"
 echo "Setting up dnf.conf..."
 
-command cp -r "$CONFIG_DIR/dnf.conf" "/etc/dnf/dnf.conf"
-chown root "/etc/dnf/dnf.conf"
-chmod 644 "/etc/dnf/dnf.conf"
+copy-dnf 
 
 echo "Finished copying dnf.conf"
 
@@ -224,7 +196,7 @@ dnf-install "$INSTALLABLE_CODECS"
 dnf-install-group "--with-optional Multimedia"
 
 case "btrfs" in
-    "$ROOT_FS" | "$REAL_USER_HOME_FS" |  "$SCRIPT_DIR_FS")
+    "$ROOT_FS" | "$REAL_USER_HOME_FS")
         echo "Found BTRFS, installing tools"
         dnf-install "$INSTALLABLE_BTRFS_TOOLS"
         ;;
@@ -328,48 +300,11 @@ done
 
 echo "-------------------INSTALLING RC FILES----------------"
 
-touch "$REAL_USER_HOME/.bashrc_private"
-change-ownership "$REAL_USER_HOME/.bashrc_private"
+create-private-bashrc
 
-ln -sf "$RC_DIR/.vimrc" "$REAL_USER_HOME/.vimrc"
-ln -sf "$RC_DIR/.bashrc" "$REAL_USER_HOME/.bashrc"
-ln -sf "$RC_DIR/.nanorc" "$REAL_USER_HOME/.nanorc"
-ln -sf "$CONFIG_DIR/.gitconfig" "$REAL_USER_HOME/.gitconfig"
-change-ownership "$REAL_USER_HOME/.vimrc" "$REAL_USER_HOME/.bashrc" "$REAL_USER_HOME/.nanorc" "$REAL_USER_HOME/.gitconfig"
+copy-rc-files
 
-mkdir -p "$CLONED_ROOT/mono-firefox-theme"
-echo "Created $CLONED_ROOT/mono-firefox-theme/"
-readonly RC_VIS_MZL_DIR="$CLONED_ROOT/mono-firefox-theme"
-# if something goes wrong, install the next version, otherwise break
-wget -c --read-timeout=5 --tries=0 --directory-prefix "$CLONED_ROOT/" "https://github.com/witalihirsch/Mono-firefox-theme/releases/download/0.5/mono-firefox-theme.tar.xz"
-tar -xf "$CLONED_ROOT/mono-firefox-theme.tar.xz" --directory="$RC_VIS_MZL_DIR"
-echo "Extracted $CLONED_ROOT/mono-firefox-theme.tar.xz"
-rm -vf "$CLONED_ROOT/mono-firefox-theme.tar.xz"
-cat "$RC_MZL_DIR/userChrome.css" >> "$RC_VIS_MZL_DIR/userChrome.css"
-echo "Installing visual rc files from $RC_VIS_MZL_DIR"
-
-#https://askubuntu.com/questions/239543/get-the-default-firefox-profile-directory-from-bash
-if [[ $(grep '\[Profile[^0]\]' "$MZL_ROOT/profiles.ini") ]]; then
-    readonly PROFPATH=$(grep -E '^\[Profile|^Path|^Default' "$MZL_ROOT/profiles.ini" | grep '^Path' | cut -c6- | tr " " "\n")
-else
-    readonly PROFPATH=$(grep 'Path=' "$MZL_ROOT/profiles.ini" | sed 's/^Path=//')
-fi
-
-for MZL_PROF_DIR in $PROFPATH; do
-    MZL_PROF_DIR_ABSOLUTE="$MZL_ROOT/$MZL_PROF_DIR"
-    MZL_PROF_CHROME_DIR_ABSOLUTE="$MZL_PROF_DIR_ABSOLUTE/chrome"
-
-    # preference rc
-    ln -sf "$RC_MZL_DIR/user.js" "$MZL_PROF_DIR_ABSOLUTE/user.js"
-    change-ownership "$MZL_PROF_DIR_ABSOLUTE/user.js"
-
-    # visual rc
-    mkdir -p "$MZL_PROF_CHROME_DIR_ABSOLUTE"
-    ln -sf "$RC_VIS_MZL_DIR/userChrome.css" "$MZL_PROF_CHROME_DIR_ABSOLUTE/userChrome.css"
-    ln -sf "$RC_VIS_MZL_DIR/userContent.css" "$MZL_PROF_CHROME_DIR_ABSOLUTE/userContent.css"
-    ln -sf "$RC_VIS_MZL_DIR/mono-firefox-theme" "$MZL_PROF_CHROME_DIR_ABSOLUTE/mono-firefox-theme"
-done
-echo "Finished installing rc files."
+copy-ff-rc-files
 
 #######################################################################################################
 
