6c640bbdfdc20b9e77189e0b3c1c5dcb0709a7c1
gerelef <44060598+gerelef@users.noreply.github.com>
2023-11-13 16:36:28 +0200

    added skeleton

--- scripts/utils/fcolour.py
+++ scripts/utils/fcolour.py
@@ -2,90 +2,98 @@
 from enum import StrEnum
 from os import environ
 from pathlib import PosixPath
-from typing import Callable
+from typing import Callable, Iterator
 from functools import partial
 
 type Colour = str
 
 
 def __colour_string(text: str, c: Colour):
-    return f"\033[{c}m{text}\033[0m"
+    return f"\033[{c}m{text}\033[0m" if c else text
 
 
 def __colour_default(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_file(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_directory(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_symlink(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_named_pipe(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_block(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_char(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_orphan(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_socket(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_setuid(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_setgid(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_sticky_other_writable(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_other_writable(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_sticky(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_exec(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_missing(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_leftcode(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_rightcode(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
 
 
 def __colour_endcode(p: PosixPath, colour: Colour):
-    pass
+    return __colour_string(p.name, colour)
+
+
+def __cascade_specials(p: PosixPath, specials_iterator: Iterator):
+    try:
+        _, fn = next(specials_iterator)
+        return fn(p)
+    except StopIteration:
+        return ""
 
 
 def colour_path(p: PosixPath):
@@ -94,12 +102,12 @@ def colour_path(p: PosixPath):
             extl = p.name.split(".")[1:]
             ext = ".".join(extl)
             # FIXME somehow after this, continue with the special keys
-            return __extension_colours[ext](p.name)
+            return __extension_colour_functions[ext](p.name)
     except KeyError:
         pass
 
     # check all special keys and return
-    return p.name
+    return __cascade_specials(p, enumerate(__special_colour_functions))
 
 
 # there are more options, but they're undocumented as far as I can tell;
@@ -127,8 +135,8 @@ __ls_colors_special_keys = {
     "ec": __colour_endcode,
 }
 
-__special_colours: list[Callable[[str], str]] = []
-__extension_colours: dict[str, Callable[[str], str]] = {}
+__special_colour_functions: list[Callable[[str], str]] = []
+__extension_colour_functions: dict[str, Callable[[str], str]] = {}
 
 _ls_colors = environ["LS_COLORS"].split(":")
 for colour_set in _ls_colors:
@@ -136,11 +144,15 @@ for colour_set in _ls_colors:
     if colour_set:
         _key, _colour = colour_set.replace("*", "").split("=")
         if _key.startswith("."):
-            __extension_colours[_key] = lambda n: __colour_string(n, _colour)
+            __extension_colour_functions[_key] = lambda n: __colour_string(n, _colour)
             continue
 
         try:
-            __special_colours.append(partial(__ls_colors_special_keys[_key], colour=_colour))
+            __special_colour_functions.append(
+                partial(
+                    __ls_colors_special_keys[_key], colour=_colour
+                )
+            )
         except KeyError:
             # silently ignore errors
             pass
