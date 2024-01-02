20a771e0da29e1ad2f23de93a57f4b5ae95108b6
gerelef <44060598+gerelef@users.noreply.github.com>
2024-01-02 13:25:58 +0200

    enabled switch-windows functionality for all workspaces

--- scripts/setup/fedora-39.sh
+++ scripts/setup/fedora-39.sh
@@ -1031,6 +1031,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.shell.window-switcher current-workspace-only false
 gsettings set org.gnome.desktop.wm.keybindings switch-group "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-group-backward  "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super>space']"
