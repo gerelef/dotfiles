a6b99b0d89cfb3d2f59432050b29d7e75786d413
gerelef <44060598+gerelef@users.noreply.github.com>
2024-02-28 01:31:29 +0200

    added more default discriminators

--- scripts/utils/modules/sela/stages/asset_discriminator.py
+++ scripts/utils/modules/sela/stages/asset_discriminator.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import override
+from typing import override, Callable
 
 from modules.sela.definitions import Filename, URL
 from modules.sela.releases.abstract import Release
@@ -19,6 +19,51 @@ class AllInclusiveAssetDiscriminator(AssetDiscriminator):
     """
     Default class that will, by default, take all the (valid, non-null) assets and return them.
     """
+
     @override
     def discriminate(self, release: Release) -> dict[Filename, URL]:
         return dict(filter(lambda s: bool(s[1]), release.assets.items()))
+
+
+class KeywordAssetDiscriminator(AssetDiscriminator):
+    """
+    Default class that will, by default, take all the assets that match all keywords and return them.
+    """
+
+    def __init__(self, *kws: str, preprocess: Callable[[str], str] = lambda s: s, strict=False):
+        """
+        @param preprocess: Preprocessor before each check.
+        """
+        self.kws = kws
+        self.prep = preprocess
+        self.strict = strict
+
+    @override
+    def discriminate(self, release: Release) -> dict[Filename, URL]:
+        check = all if self.strict else any
+        td = {}
+        for fn, url in release.assets.items():
+            evaluations = [keyword in self.prep(fn) for keyword in self.kws]
+            if check(evaluations):
+                td[fn] = url
+        return td
+
+
+class LambdaAssetDiscriminator(AssetDiscriminator):
+    """
+    Default class that will, by default, take all the assets that match at least one lambda and return them.
+    """
+
+    def __init__(self, *lds: Callable[[str, str], bool], strict=False):
+        self.lambdas = lds
+        self.strict = strict
+
+    @override
+    def discriminate(self, release: Release) -> dict[Filename, URL]:
+        check = all if self.strict else any
+        td = {}
+        for fn, url in release.assets.items():
+            evaluations = [evaluator(fn, url) for evaluator in self.lambdas]
+            if check(evaluations):
+                td[fn] = url
+        return td
--- scripts/utils/modules/sela/stages/release_discriminator.py
+++ scripts/utils/modules/sela/stages/release_discriminator.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import override
+from typing import override, Callable
 
 from modules.sela.releases.abstract import Release
 
@@ -24,14 +24,35 @@ class FirstReleaseDiscriminator(ReleaseDiscriminator):
         return True
 
 
-class SimpleMatchDiscriminator(ReleaseDiscriminator):
+class KeywordReleaseDiscriminator(ReleaseDiscriminator):
     """
-    Default class that will, by default, accept the first release that contains the matched text.
+    Default class that will, by default, accept the first release that contains any of the matched text(s).
     """
 
-    def __init__(self, match):
-        self.match = match
+    def __init__(self, *kws, preprocess: Callable[[str], str] = lambda s: s, strict=False):
+        """
+        @param preprocess: Preprocessor before each check.
+        """
+        self.kws = kws
+        self.prep = preprocess
+        self.strict = strict
 
     @override
     def discriminate(self, release: Release) -> bool:
-        return self.match in release.name_human_readable
+        check = all if self.strict else any
+        return check([keyword in self.prep(release.name_human_readable) for keyword in self.kws])
+
+
+class LambdaReleaseDiscriminator(ReleaseDiscriminator):
+    """
+    Default class that will, by default, accept the first release that fulfills all lambdas' requirements.
+    """
+
+    def __init__(self, *lds: Callable[[Release], bool], strict=False):
+        self.lambdas = lds
+        self.strict = strict
+
+    @override
+    def discriminate(self, release: Release) -> bool:
+        check = all if self.strict else any
+        return check([evaluator(release) for evaluator in self.lambdas])
