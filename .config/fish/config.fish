9aec3ee3a9d52a1279ed6c75b6cc549abc355252
gerelef <44060598+gerelef@users.noreply.github.com>
2024-02-28 21:39:44 +0200

    added required login packages to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path -g ~/dotfiles/scripts/functions/
+
+    require-login-shell-packages
+
     set -g fish_greeting ""
     alias lss "lsd --almost-all --icon never --icon-theme unicode --group-directories-first"
 end
