5574ab18f0fbe29c227a8304f334cffa75e1a73c
gerelef <44060598+gerelef@users.noreply.github.com>
2024-03-08 23:57:02 +0200

    added common alias to fishrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,26 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    #############################################################
+    # SOURCES & CONFIG
+    set -g fish_greeting ""
+    # add executable script (lambdas) dir 
     fish_add_path -g ~/dotfiles/scripts/functions/
-
+    # add requirements
     require-login-shell-packages
 
-    set -g fish_greeting ""
+    #############################################################
+    # ALIAS
+    alias .. "cd .."
+    alias ... "cd ../.."
+    alias .... "cd ../../.."
+    alias ..... "cd ../../../.."
+
     alias lss "lsd --almost-all --icon never --icon-theme unicode --group-directories-first"
-    alias ... "cd ../../"
-    alias .... "cd ../../../"
-    alias ..... "cd ../../../../"
+    alias wget "\wget -c --read-timeout=5 --tries=0"
+    alias grep "\grep -i"
+    alias rm "rm -v"
+    alias reverse "tac"
+    alias palindrome "rev"
+    
+    alias fuck "sudo $history[2]"
 end
