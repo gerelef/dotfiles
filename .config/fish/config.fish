179bdaf9e4501d9cd7eb5fcc3321288f47a26c7d
gerelef <44060598+gerelef@users.noreply.github.com>
2024-03-09 18:14:10 +0200

    added autogenerated meta-virtual subshell pip alias/functions for fish (implemented from bashrc)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,56 +20,73 @@ if not status is-interactive; exit; end;
 # important note: the statement pythonX.x -m venv \"\$venv_dir\" won't work with 2.7 or lower,
 #  for that, we need the virtualenv module
 function prepare-pip
-    # vpip_fname="/tmp/vpip-temp-$(date +%s%N).sh"
-    # venv_dir="$HOME/.vpip"
-    # python_versions=()
+    set vpip_fname "/tmp/vpip-temp-$(date +%s%N).fish"
+    set venv_dir "$HOME/.vpip"
 
     # # get all the appropriate versions from the filesystem
     # # https://stackoverflow.com/a/57485303
-    # for pv in "$(ls -1 /usr/bin/python* | grep '.*[0-9]\.\([0-9]\+\)\?$' | sort --version-sort)"; do
-    #     python_versions+=("$pv")
-    # done
+    # set array
+    # access elements with $array[INDEX]
+    set -l python_versions
+    for pv in (ls -1 /usr/bin/python* | grep '.*[0-9]\.\([0-9]\+\)\?$' | sort --version-sort)
+        set -a python_versions "$pv" # append to array
+    end
 
-    # # create mock functions
-    # for python_version in $python_versions; do
-    #     # sanitize the filename and keep only the numbers at the end
-    #     python_version_number="$(echo $python_version | tr -d -c 0-9.)"
+    # create mock functions
+    for pv in $python_versions
+        # sanitize the filename and keep only the numbers at the end
+        set pv_num $(echo $pv | tr -d -c 0-9.)
 
-    #     virtual_group_subshell="vpip$python_version_number () {
-    #         [[ \"\$EUID\" -eq 0 ]] && echo \"Do NOT run as root.\" && return 2; 
-    #         [[ ! -d \"$venv_dir\" ]] && mkdir -p \"$venv_dir\" # create root dir if doesn't exist
-    #         local venv_dir=\"$venv_dir/dvpip$python_version_number\"
+        set virtual_subshell "\
+        function vpip$pv_num
+            # don't run if it's root
+            if test \"$EUID\" -eq 0
+                echo \"Do NOT run as root!\"
+                return 2
+            end
+            # create root dir if doesn't exist
+            if not test -d \"$venv_dir\"
+                echo \"Root $venv_dir doesn't exist; Creating it...\"
+                mkdir -p \"$venv_dir\"
+            end
 
-    #         # if venv dir doesn't exist for our version create it
-    #         if [[ ! -d \"\$venv_dir\" ]]; then
-    #             echo \"\$venv_dir doesn't exist; creating venv for $python_version\"
+            # if venv dir doesn't exist for our version create it
+            set pv_dir \"$venv_dir/dvpip$pv_num\"
+            if not test -d \"\$pv_dir\"
+                echo \"Global \$pv_dir doesn't exist; creating venv for it!\"
+                # the $pv expansion will execute, since it's expanded while making
+                #  the stringified version of this function
+                # special case of venv creation: 
+                #  if python version < 3 (2.7 e.g.) use a special way for init
 
-    #             # special case of the trick below: if python version < 3 (2.7 e.g.) use a special way for venv init
-    #             if [[ ${python_version_number%.*} -lt 3 ]]; then
-    #                 $python_version  -m ensurepip --user
-    #                 $python_version -m pip install virtualenv --user
-    #                 $python_version -m virtualenv --python=\"$python_version\" \"\$venv_dir\"
-    #             else
-    #                 $python_version -m venv \"\$venv_dir\" # for python >= 3
-    #             fi
-    #         fi
+                # split on the dot (MAJOR.MINOR version spliterator)
+                #  if there's no dot (???) in the version try to use pv as is
+                set major_pv_num $(string split . $pv_num)[1]; or set major_pv_num $pv_num
+                if test \$major_pv_num -lt 3
+                    command $pv -m ensurepip --user
+                    command $pv -m pip install virtualenv --user
+                    command $pv -m virtualenv --python=\"$pv\" \"\$pv_dir\"
+                else 
+                    # for python >= 3
+                    command $pv -m venv \$pv_dir
+                end
+            end
+            fish --init-command \"source \$pv_dir/bin/activate.fish\"
+        end
+        "
+        # append to the file
+        echo "$virtual_subshell" >> "$vpip_fname"
+    end
 
-    #         bash --init-file <(echo \"source \\\"$HOME/.bashrc\\\"; source \$venv_dir/bin/activate\")
-    #     }"
-
-    #     # append to the file
-    #     echo "$virtual_group_subshell" >> $vpip_fname
-    # done
-
-    # echo "$vpip_fname"
+    echo "$vpip_fname"
 end
 
 function require-pip
-    # local vpip_fname="$(prepare-pip)"
+    set vpip_fname "$(prepare-pip)"
 
-    # # source the file & delete
-    # source "$vpip_fname"
-    # rm "$vpip_fname"
+    # source the file & delete
+    source "$vpip_fname"
+    rm "$vpip_fname"
 end
 
 #############################################################
@@ -86,11 +103,12 @@ set -g fish_greeting ""
 #############################################################
 # ABBREVIATIONS & ALIAS
 abbr -a lss "lsd --almost-all --icon never --group-directories-first"
-abbr -a wget "\wget -c --read-timeout=5 --tries=0"
-abbr -a grep "\grep -i"
+abbr -a wget "wget -c --read-timeout=5 --tries=0"
+abbr -a grep "grep -i"
 abbr -a rm "rm -v"
 abbr -a reverse "tac"
 abbr -a palindrome "rev"
+abbr -a unset "set --erase"
 
 alias .. "cd .."
 alias ... "cd ../.."
