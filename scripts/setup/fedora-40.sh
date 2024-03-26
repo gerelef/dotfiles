a57d99ad3fb2289532770052140d74da6c5d3e7f
gerelef <44060598+gerelef@users.noreply.github.com>
2024-03-26 21:25:43 +0200

    added fedora 40 skeleton install script

---
+++ scripts/setup/fedora-40.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S sudo --preserve-env="XDG_CURRENT_DESKTOP" --preserve-env="XDG_RUNTIME_DIR" --preserve-env="XDG_DATA_DIRS" --preserve-env="DBUS_SESSION_BUS_ADDRESS" bash
+
+readonly DIR=$(dirname -- "$BASH_SOURCE")
+
+[[ -f "$DIR/common-utils.sh" ]] || ( echo "$DIR/common-utils.sh doesn't exist! exiting..." && exit 2 ) 
+source "$DIR/common-utils.sh"
+
+# DEPENDENCIES FOR THE CURRENT SCRIPT 
+dnf-install flatpak curl plocate pciutils udisks2 dnf5
+
+# change dnf4 to dnf5 (preview/unstable: is supposed to be shipped with fedora-41)
+update-alternatives --install /usr/bin/dnf dnf /usr/bin/dnf5 1
+
