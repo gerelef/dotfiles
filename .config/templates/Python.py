1bbb00313bd1369de16681f133613b49071fd7ad
gerelef <44060598+gerelef@users.noreply.github.com>
2024-10-02 00:27:38 +0300

    soft dependency of python3.12

--- .config/templates/Python.py
+++ .config/templates/Python.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S python3 -S -OO
+import sys
+
+
+if sys.version_info.minor < 12:
+    print(">= python3.12 required!", file=sys.stderr)
+    sys.exit(1)
 
 if __name__ == "__main__":
     print("Hello World!")
