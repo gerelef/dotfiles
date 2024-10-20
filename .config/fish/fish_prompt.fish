2aa44747a3ad6d0344d97c12c07e25abd6296742
gerelef <44060598+gerelef@users.noreply.github.com.>
2024-10-20 23:40:36 +0300

    big oops

--- .config/fish/fish_prompt.fish
+++ .config/fish/fish_prompt.fish
@@ -64,7 +64,7 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " " $prompt_status $suffix " "
+    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
 end
 
 function fish_right_prompt -d "Write out the right prompt"
